aab336df51c931fb9017fe70ac035071cb2aa6b4
Chirag Tailor <ctailor2@gmail.com>
2014-02-02 19:12:37 -0600

    Initial commit - fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeddingSpike::Application.config.secret_key_base = 'b85f7ed024cb80323d948e8a1ead7ce0269f13ed256b1e29a4cebefdaad8d771647a7b801254d3de970d0c9bf40516188ffec63e530c5db1a56b60f83119cbad'
